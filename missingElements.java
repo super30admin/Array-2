d5a455c3b057df9e11e83ac7b691a2c2ea91ee87
Ajith Balaji <ajithbalajiun@gmail.com>
2019-09-03 22:25:38 -0700

    added comments to the previously pushed code - Please review

--- missingElements.java
+++ missingElements.java
@@ -1,3 +1,8 @@
+//Time Complexity: O(N)
+//SPace Complexity:O(1)
+//In this problem, first the length of the array is traversed through and then at each iteration, the element at the index -1 corresponding to the element at the current iteration is changed to negative if it is grater than zero. Then the array is again iterated through and those indices for which the element values are greater than zero are returned as index +1.
+//This problem was successfully executed and got accepted in leetcode.
+
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         List<Integer> out=new ArrayList<Integer>();

6b9218e8edffe495e6583cd3651cd9a8293a903d
rutulusc <rutulbap@usc.edu>
2020-01-08 20:30:14 -0800

    Completed 30,31,32 problems

---
+++ prob31.java
@@ -0,0 +1,9 @@
+// S30 Big N Problem #31 {Easy}
+// An array of numbers of length N is given , you need to find the minimum and maximum. try doing this in less than 2* (N-2) comparisons
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode : NA
+// Any problem you faced while coding this :None
+
+
+// Your code here along with comments explaining your approach
---
+++ prob32.java
@@ -0,0 +1,9 @@
+// S30 Big N Problem #32 {Medium}
+// 289. Game of Life
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :None
+
+
+// Your code here along with comments explaining your approach

59dd4593fcdd44e8603a07ef9bf4273412ca38bd
arcsoftech <arihant_Chhajed@yahoo.in>
2020-03-31 21:57:33 -0500

    Added  in condition pattern

--- GanmeOfLife.java
+++ GanmeOfLife.java
@@ -10,13 +10,13 @@ class Solution {
     public void gameOfLife(int[][] board) {
         //edge case
         if(board==null || board.length==0) return;
-        
+     int [][] dirs = new int[][]{{1,0},{-1,0},{0,1},{0,-1},{-1,-1},{1,1},{-1,1},{1,-1}};
      
         for(int i = 0 ; i <board.length;i++)
         {
             for(int j = 0 ; j <board[0].length;j++)
             {
-                int live = neighbour(board,i,j);
+                int live = neighbour(board,i,j,dirs);
                 //live cell
                 if(board[i][j]==1)
                 {
@@ -43,31 +43,22 @@ class Solution {
         }
     
     }
-    private int neighbour(int[][] board,int i , int j)
+    private int neighbour(int[][] board,int i , int j,int[][] dirs)
     {
         int count=0;
-        //up
-        if(i-1>=0)
+        
+        for(int[] d:dirs)
         {
-            count+=board[i-1][j]==5 || board[i-1][j]==1 ?1:0;
-            if(j+1<board[0].length) count+=board[i-1][j+1]==5 || board[i-1][j+1]==1 ?1:0;
-            if(j-1>=0) count+=board[i-1][j-1]==5 || board[i-1][j-1]==1 ?1:0;
-        }
-        if(i+1<board.length) // down
-        {
-            count+=board[i+1][j]==5 || board[i+1][j]==1 ?1:0;
-            if(j+1<board[0].length) count+=board[i+1][j+1]==5 || board[i+1][j+1]==1 ?1:0;
-            if(j-1>=0) count+=board[i+1][j-1]==5 || board[i+1][j-1]==1 ?1:0;
+            int r = i+ d[0];
+            int c = j+d[1];
+            if(r >=0 && r <board.length && c>=0 && c <board[0].length)
+            {
+                if(board[r][c]==5 || board[r][c]==1)
+                    count+=1;
+            }
             
-        }
-        if(j-1>=0) //left
-        {
-            count+=board[i][j-1]==5 || board[i][j-1]==1 ?1:0;
-        }
-        if(j+1<board[0].length) //right
-        {
-            count+=board[i][j+1]==5 || board[i][j+1]==1 ?1:0;
         }
         return count;
     }
+
 }

f043f91178504d27a812b8f8b36543a3102230ae
Akshay-Ravi <akshaynitt97@gmail.com>
2020-05-10 17:11:55 +0530

    Updated the 3 point algo

--- find_missing_number.cpp
+++ find_missing_number.cpp
@@ -2,6 +2,11 @@
 //Space Complexity - O(1)
 //Successfully ran on leetcode
 //Use this pattern of making the corresponding numbers negative to mutate the initial state of the array
+//The algorithm is as follows -
+/* 1. We will go through the array and make the corresponding index of that value negative. For example, nums[0]=4 implies that we go to
+      index 4-1=3, and make the element at nums[3] negative. This tells us that number 4 is present.
+   2. Once we do this for the entore array, we can just push into the result array the indices of those elements in nums array, which are
+      still positive. That will give us those numbers that are missing. */
 
 class Solution {
 public:

3bc9c6477b992f079526298ffea216bebce37fe7
ashiranka <ashiranka@gmail.com>
2020-06-09 06:47:04 -0700

    Completed

--- FindMinMaxInArray.java
+++ FindMinMaxInArray.java
@@ -6,7 +6,7 @@
 
 import java.io.*;
 
-class Solution {
+/*class Solution {
 	public static void main (String[] args) {
 		int[] arr = {2,5,9,-3,6,-7};
 		int[] result = minmax(arr);
@@ -59,3 +59,77 @@ class Solution {
 	   
 	}
 }
+*/
+
+
+
+
+
+//less than 2(n-1) comparisions
+//3*(n/2)-2 Comparisons if even
+//3*(n-1)/2  comparisons if odd
+//Pair wise comparisons
+class Solution { 
+  
+    public static int[] findMinMax(int arr[], int n) { 
+        int[] result = new int[2];
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int i; 
+        
+		//if even no. of elements
+        if (n % 2 == 0) { 
+            if (arr[0] > arr[1]) { 
+                max = arr[0]; 
+                min = arr[1]; 
+            } else { 
+                min = arr[0]; 
+                max = arr[1]; 
+            } 
+            i = 2; 
+            
+        } 
+		
+		//if odd no. of elemets
+        else { 
+            min = arr[0]; 
+            max = arr[0]; 
+            i = 1; 
+        } 
+  
+       //pair wise comparisons with even and odd
+        while (i < n - 1) { 
+            if (arr[i] > arr[i + 1]) { 
+                if (arr[i] > max) { 
+                    max = arr[i]; 
+                } 
+                if (arr[i + 1] < min) { 
+                    min = arr[i + 1]; 
+                } 
+            } else { 
+                if (arr[i + 1] > max) { 
+                    max = arr[i + 1]; 
+                } 
+                if (arr[i] < min) { 
+                    min = arr[i]; 
+                } 
+            } 
+            i += 2; //inc i count by 2 
+            
+        } 
+        
+		result[0] = min;
+        result[1] = max;
+  
+        return result; 
+    } 
+  
+    
+    public static void main(String args[]) { 
+        int arr[] = {2,6,8,1,10,250}; 
+        int arrsize = 6; 
+        int[] minmax = findMinMax(arr, arrsize); 
+        System.out.println("Minimum element is " +minmax[0]); 
+        System.out.println("Maximum element is " +minmax[1]); 
+    } 
+} 

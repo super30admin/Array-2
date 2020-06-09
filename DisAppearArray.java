b7dfad852bd76ce584d180b05d9c8be7e1a2d546
chirag <cr.tagadiya@gmail.com>
2020-06-09 14:47:20 -0400

    DisappearArray

--- DisAppearArray.java
+++ DisAppearArray.java
@@ -1,13 +1,19 @@
+import java.util.HashSet;
 /*
-    Time Complexity : O(N)
-    Space Complexity:O(N)
-    is Worked on leetcode : YEs
+    BruteForce Solution
+        Time Complexity : O(N)
+        Space Complexity:O(N)
+        is Worked on leetcode : YEs
+    Efficient Solution
+        Time Complexity :O(N)
+        Space Complexity :  O(1)
+        is workedon leetcode : YES
 */
 
 import java.util.List;
 import java.util.ArrayList;
 public class DisAppearArray{
-    public List<Integer> findDisappearedNumbers(int[] nums) {
+    public List<Integer> findDisappearedNumbers_Bruteforce(int[] nums) {
         ArrayList<Integer> al = new ArrayList<Integer>();
         if( nums == null || nums.length == 0 ) return al;
         
@@ -21,5 +27,31 @@ public class DisAppearArray{
             }
         }
         return al;
+    }
+
+
+    public List<Integer> findDisappearedNumbers1(int[] nums) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        if( nums == null || nums.length == 0 ) return al;
+        for(int i=0; i<nums.length;i++){
+           int index = Math.abs(nums[i])-1;
+           if(nums[index] > 0){
+               nums[index] = nums[index]*-1;
+           }
+        }
+        
+        for(int i=0; i<nums.length;i++){
+            if(nums[i]>0){
+                al.add(i+1);
+            }
+        }
+        
+        return al;
+        
+    }
+    public static void main(String[] args) {
+        DisAppearArray obj =  new DisAppearArray();
+        int[]nums= new int[]{4,3,2,7,8,2,3,1};
+        System.out.println(obj.findDisappearedNumbers1(nums).toString());
     }
 }

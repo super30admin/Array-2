87b21685bf2c4345e2cc2fe0d8912ad8cfc057b5
Sarthak Sugandhi <47070701+sarthaksugandhi9@users.noreply.github.com>
2020-06-09 17:19:07 -0700

    Create Problem_2.java

---
+++ Problem_2.java
@@ -0,0 +1,36 @@
+// Time complexity - O(n) .. O(3n/2) approx
+// Space complexity - O(1)
+
+// Compare elements in pair and update max and min variables.
+
+
+public class MinMaxWithReducedIterations {
+
+	public static void main(String[] args) {
+		int n, max, min;
+		n = 7;
+		int[] arr = new int[] { 2, 8, 6, 2, 11, 5, 70 };
+		max = arr[0];
+		min = arr[0];
+		for (int i = 0; i < n; i = i + 2) {
+			if (i == n - 1) {
+				if (arr[i] > max)
+					max = arr[i];
+				if (arr[i] < min)
+					min = arr[i];
+			} else if (arr[i] > arr[i + 1]) {
+				if (arr[i] > max)
+					max = arr[i];
+				if (arr[i + 1] < min)
+					min = arr[i + 1];
+			} else {
+				if (arr[i + 1] > max)
+					max = arr[i + 1];
+				if (arr[i] < min)
+					min = arr[i];
+			}
+		}
+		System.out.println(max);
+		System.out.println(min);
+	}
+}

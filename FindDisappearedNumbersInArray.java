fd061792e4675d32004769dd33e82c848ebc642e
Arpit Sharma <arpit317@gmail.com>
2020-07-25 18:28:48 -0700

    Update FindDisappearedNumbersInArray.java

--- FindDisappearedNumbersInArray.java
+++ FindDisappearedNumbersInArray.java
@@ -38,3 +38,42 @@ class Solution {
         return res;
     }
 }
+
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(nums==null || nums.length == 0) return res;
+        int[] nums2 = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            nums2[i] = i+1;
+        }
+        
+         for(int i = 0; i < nums.length; i++){
+            nums2[nums[i]-1] = 0;
+        }
+          
+        int low = 0;
+        int high = nums2.length-1;
+        while(low <= high){
+            if(nums2[high] != 0){
+                int temp = nums2[low];
+                nums2[low] = nums2[high];
+                nums2[high] = temp;
+                low++;
+            }
+            else{
+                high--;
+            }
+        }
+        
+        int pointer = nums2.length-1;
+        while(pointer >= 0 && nums2[pointer]==0){
+            pointer--;
+        }
+        for(int i = 0; i <= pointer;i++){
+            res.add(nums2[i]);
+        }
+        return res;
+    }
+}

0acd5c0f1f8233b682b89962010d7bf0385dc57a
Arpit Sharma <arpit317@gmail.com>
2020-07-25 18:42:42 -0700

    Update FindDisappearedNumbersInArray.java

--- FindDisappearedNumbersInArray.java
+++ FindDisappearedNumbersInArray.java
@@ -77,3 +77,22 @@ class Solution {
         return res;
     }
 }
+
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList();
+        for(int i = 0; i < nums.length; i++){
+            if(nums[Math.abs(nums[i])-1]>0){
+                nums[Math.abs(nums[i])-1] *= -1;
+            }
+        }
+        
+        for(int i = 0; i < nums.length; i++){
+           if(nums[i]>0){
+               res.add(i+1);
+           }
+        }
+        return res;
+    }
+}

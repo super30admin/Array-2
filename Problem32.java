020edba8723c1486524fd0eedcdf4f0d0a0d87a3
ramya-sruthi-d <30996689+ramya-sruthi-d@users.noreply.github.com>
2020-09-01 18:56:19 -0500

    Add files via upload

--- Problem32.java
+++ Problem32.java
@@ -1,5 +1,5 @@
 //Time Complexity: O(n*m)
-//Space Complexity: O(n*m)
+//Space Complexity: O(1)
 //Did it run on leetcode: yes
 //Problems faced any: No
 
@@ -9,7 +9,6 @@ import java.util.List;
 
 public class Problem32 {
     public void gameOfLife(int[][] board) {
-        int[][] arr = new int[board.length][board[0].length];
 
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
@@ -25,21 +24,26 @@ public class Problem32 {
                 list.add(getValue(i-1,j-1,board));
                 if(board[i][j]==1){
                     if(calculateStateforOne(list) == false)
-                        arr[i][j]=0;
-                    else
-                        arr[i][j]=1;
+                        //to keep track of what elements have changed
+                        //this line means 1 is changed to 0
+                        board[i][j]=2;
                 }else{
-                    if(calculateStateforZero(list) == false)
-                        arr[i][j]=0;
-                    else
-                        arr[i][j]=1;
+                    if(calculateStateforZero(list) == true)
+                        //to keep track of what elements have changed
+                        //this line means 0 is changed to 1
+                        board[i][j]=3;
                 }
             }
         }
 
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr[0].length; j++){
-                board[i][j] = arr[i][j];
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                if(board[i][j]==2)
+                    board[i][j]=0;
+                else if(board[i][j]==3)
+                    board[i][j]=1;
+                else
+                    continue;
             }
         }
 
@@ -74,8 +78,14 @@ public class Problem32 {
     }
 
     public int getValue(int i, int j, int[][] board){
-        if((i>=0 && i<board.length) && (j>=0 && j<board[0].length))
-            return board[i][j];
+        if((i>=0 && i<board.length) && (j>=0 && j<board[0].length)){
+            if(board[i][j]==2)
+                return 1;
+            else if(board[i][j]==3)
+                return 0;
+            else
+                return board[i][j];
+        }
         return 0;
     }
 }

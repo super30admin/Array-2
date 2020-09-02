3357df5d31cc7bf5ac1dd695a76bb081ea423b31
vatsaashwin <aashwinvats@GMAIL.com>
2020-09-02 01:41:40 -0700

    disappearedArr O(1)

--- findDisapperadNums.py
+++ findDisapperadNums.py
@@ -24,4 +24,37 @@ class Solution:
                 
         return result
         
+# ---Solution with O(1) space, discussed in class----#
+
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+# Approach: Discussed in class
+# Your code here along with comments explaining your approach
+# This involves manipulating the input array
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        
+        if nums == None or len(nums)==0:
+            return []
+        
+        result = []
+        
+        for i in range(len(nums)):
+            #if the value at current index is non-negative, go to the index that equals its value
+            index = abs(nums[i])-1
+            if nums[index]>0:
+                nums[index] *= -1
+            
+
+        # change the modified input array to have only negatives
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                result.append(i+1)
+                
+        return result
+
         

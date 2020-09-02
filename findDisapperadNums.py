835fa2d0a9678e41ae89b19c70e919f57c73b87c
vatsaashwin <aashwinvats@GMAIL.com>
2020-09-01 17:31:19 -0700

    disappearedArr O(n)

---
+++ findDisapperadNums.py
@@ -0,0 +1,27 @@
+# Time Complexity : O(n)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+
+# Your code here along with comments explaining your approach
+# Put the nums in a hashmap and then return the ones that are not in the hashmap from 1 to N
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        
+        hashset = {}
+        
+        result = []
+        
+        for i in range(len(nums)):
+            if nums[i] not in hashset.keys():
+                hashset[nums[i]] = i
+        
+        for n in range(1, len(nums)+1):
+            if n not in hashset.keys():
+                result.append(n)
+                
+        return result
+        
+        
---
+++ minMaxArr.py
@@ -0,0 +1,9 @@
+# Problem:
+# Given an array of numbers of length N, find both the minimum and maximum. Follow up : Can you do it using less than 2 * (N - 2) comparison
+
+# Time Complexity :
+# Space Complexity :
+# Did this code successfully run on Leetcode :
+# Any problem you faced while coding this :
+
+# Your code here along with comments explaining your approach

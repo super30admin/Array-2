ae93cf34ab299edc13ff444b2f378ee06d81d65d
pkariya1 <pkariya1@binghamton.edu>
2020-10-28 16:34:55 +0530

    Game of Life completed- Array2

--- Sample.java
+++ Sample.java
@@ -28,3 +28,23 @@
 // if there is duplicate, and its already marked negative dont change anything
 // after marking negative of particular index then whichever positive element is present in array
 //take that particular index +1 and return it..
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+//Game of life
+
+// Time Complexity : O(m*n) 
+// Space Complexity :O(1) // no auxilary space
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+//Solved it using 2 nested loops to check the lives no.of count by using the direction array
+//i.e. // int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 }, { -1, 1 }, { 1, 1 }, { -1, -1 }, { 1, -1 } };
+// Then if board is tranisted from dead to live we will use 3 
+//if board is tranisted from live to dead we will use 2 these all are according to the rules
+
+// after computating the next state of the board, then will change all 3's to 1
+//all 2's to 0.

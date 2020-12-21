a1c90f394c8c5c8595b3a79398ebd0f22e784934
Shivi_Bhatt <in.shivi@gmail.com>
2020-12-21 13:35:24 -0800

    Done

--- Sample.java
+++ Sample.java
@@ -1,7 +1,74 @@
-// Time Complexity :
-// Space Complexity :
+// Time Complexity :O(N)
+// Space Complexity : O(1)
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 
 
 // Your code here along with comments explaining your approach
+
+ public List<Integer> findDisappearedNumbers(int[] nums) {
+        
+        // Iterate over each of the elements in the original array
+        for (int i = 0; i < nums.length; i++) {
+            
+    
+            int newIndex = Math.abs(nums[i]) - 1;
+            
+    
+            if (nums[newIndex] > 0) {
+                nums[newIndex] *= -1;
+            }
+        }
+        
+        // Response array that would contain the missing numbers
+        List<Integer> result = new LinkedList<Integer>();
+        
+        // Iterate over the numbers from 1 to N and add all those
+        // that have positive magnitude in the array
+        for (int i = 1; i <= nums.length; i++) {
+            
+            if (nums[i - 1] > 0) {
+                result.add(i);
+            }
+        }
+        
+        return result;
+    }
+
+
+
+//=====================================================================
+
+ public void gameOfLife(int[][] board) {
+
+        // Neighbors array to find 8 neighboring cells for a given cell
+        int[] neighbors = {0, 1, -1};
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // Iterate through board cell by cell.
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+
+                // For each cell count the number of live neighbors.
+                int liveNeighbors = 0;
+
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+
+                        if (!(neighbors[i] == 0 && neighbors[j] == 0)) {
+                            int r = (row + neighbors[i]);
+                            int c = (col + neighbors[j]);
+
+                            // Check the validity of the neighboring cell.
+                            // and whether it was originally a live cell.
+                            if ((r < rows && r >= 0) && (c < cols && c >= 0) && (Math.abs(board[r][c]) == 1)) {
+                                liveNeighbors += 1;
+                            }
+                        }
+                    }
+                  
+                  //Time Complexity: O(M×N), where M is the number of rows and N is the number of columns of the Board.
+
+              //Space Complexity: O(1)

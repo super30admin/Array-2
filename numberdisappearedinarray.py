56605c66cd5decd3044a92e0d9340fa6ec850c8f
amby2964 <amby2964@gmail.com>
2021-01-06 20:36:32 -0700

    Added Minmax

--- numberdisappearedinarray.py
+++ numberdisappearedinarray.py
@@ -2,12 +2,16 @@ class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         '''
         Time: O(n)
-        Space: O(n)
+        Space: O(1)
         '''
-        n= len(nums)
-        x = set([i for i in range(1,n+1)])
-        for i in nums:
-            if(i in x):
-                x.remove(i)
-            
+        for i in range(0,len(nums)):
+            idx= abs(nums[i]) - 1
+            if(nums[idx]>0):
+                nums[idx] *= -1
+        
+        x = []
+        for i in range(0,len(nums)):
+            if(nums[i]>0):
+                x.append(i+1)
+        
         return x

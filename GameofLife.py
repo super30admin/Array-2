279f2aec1e42101470476e946f5f376bb18b2767
pranati05 <pranati05@gmail.com>
2021-01-09 00:33:04 -0800

    Completed Array-2

--- GameofLife.py
+++ GameofLife.py
@@ -1,43 +1,41 @@
 # Time Complexity : O(MN)
-# Space Complexity : O(MN)
+# Space Complexity : O(1)
 # Did this code successfully run on Leetcode : Yes
-# Any problem you faced while coding this :
+# Any problem you faced while coding this : No
 
 # Your code here along with comments explaining your approach
+# Check count of all the neighbors of each cell and assign 2 if the cell is 1 and 3 if the cell is 0.
+# Then assign 3 to 1 and 2 to 0 for the output
 
 
 class Solution:
     def gameOfLife(self, board: List[List[int]]) -> None:
-        neighbors = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1),
-                     (0, 1), (1, 1)]
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                count = self.checkNeighbors(board, i, j)
+                if board[i][j] == 1 and (count < 2 or count > 3):
+                    board[i][j] = 2
+                if (board[i][j] == 0 and count == 3):
+                    board[i][j] = 3
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == 3:
+                    board[i][j] = 1
+                if board[i][j] == 2:
+                    board[i][j] = 0
 
-        rows = len(board)
-        cols = len(board[0])
-
-        # Create a copy of the original board
-        copy_board = [[board[row][col] for col in range(cols)]
-                      for row in range(rows)]
-
-        # Iterate through board cell by cell.
-        for row in range(rows):
-            for col in range(cols):
-
-                # For each cell count the number of live neighbors.
-                live_neighbors = 0
-                for neighbor in neighbors:
-
-                    r = (row + neighbor[0])
-                    c = (col + neighbor[1])
-
-                    # Check the validity of the neighboring cell and if it was originally a live cell.
-                    # The evaluation is done against the copy, since that is never updated.
-                    if (r < rows and r >= 0) and (c < cols and c >= 0) and (
-                            copy_board[r][c] == 1):
-                        live_neighbors += 1
-
-                if copy_board[row][col] == 1 and (live_neighbors < 2
-                                                  or live_neighbors > 3):
-                    board[row][col] = 0
-
-                if copy_board[row][col] == 0 and live_neighbors == 3:
-                    board[row][col] = 1
+    def checkNeighbors(self, board, i, j):
+        count = 0
+        neighbors = [(0, 1), (0, -1), (-1, 0), (1, 0), (-1, -1), (-1, 1),
+                     (1, 1), (1, -1)]
+        for neighbor in neighbors:
+            row = i + neighbor[0]
+            col = j + neighbor[1]
+            if row >= 0 and col >= 0 and row < len(board) and col < len(
+                    board[0]) and (board[row][col] == 1
+                                   or board[row][col] == 2):
+                count += 1
+        return count

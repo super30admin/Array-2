4a73460de630dc55b0cc2e05303fbe0c5980846c
soumith <soumithr@buffalo.edu>
2021-02-02 16:55:32 -0500

    Finished Task

---
+++ gameOfLife.py
@@ -0,0 +1,36 @@
+﻿#TimeComplexity:O(MN) 
+#SpaceComplexity: Constant Space (No extra space taken).
+#Did this code successfully run on Leetcode : Yes 
+#Any problem you faced while coding this : No
+class Solution(object):
+    def gameOfLife(self, board):
+        """
+        :type board: List[List[int]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        def Count(board,i,j,m,n):
+            N=[[i-1,j-1],[i-1,j],[i-1,j+1],[i,j+1],[i+1,j+1],[i+1,j],[i+1,j-1],[i,j-1]]
+            count=0
+            for K in N:
+                if K[0]>=0 and K[1]>=0 and K[0]<m and K[1]<n and (board[K[0]][K[1]]==1 or board[K[0]][K[1]]==2) :
+                    count+=1
+            return count
+        m=len(board);n=len(board[0])
+        for i in range(m):
+            for j in range(n):
+                count=Count(board,i,j,m,n)
+                if board[i][j]==1 :
+                    if count<2 or count>3:                     
+                        board[i][j]=2
+                if board[i][j]==0 :
+                    if count==3:
+                        board[i][j]=3
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j]==2:
+                    board[i][j]=0
+                if board[i][j]==3:
+                    board[i][j]=1
+        return board
+        
---
+++ minMaxFinding.py
@@ -0,0 +1,37 @@
+﻿#TimeComplexity:O(MN) (3N/2)+2 Comparisions 
+#SpaceComplexity: Constant Space (No extra space taken).
+#Did this code successfully run on Leetcode : Yes 
+#Any problem you faced while coding this : No
+class MinMAx():
+    def MinMAxFinding(self,nums):
+        if len(nums)==0:
+            return None
+        if len(nums)==1:
+            return nums[0],nums[0]
+        if nums[0]<=nums[1]:
+            min=nums[0]
+            max=nums[1]
+        else:
+            max=nums[0]
+            min=nums[1]  
+        for i in range(1,len(nums)//2):
+            if nums[2*i]>nums[2*i+1]:
+                if nums[2*i]>max:
+                    max=nums[2*i]
+                if nums[2*i+1]<min:
+                    min=nums[2*i+1]
+            else:
+                if nums[2*i+1]>max:
+                    max=nums[2*i+1]
+                if nums[2*i]<min:
+                    min=nums[2*i]
+        if len(nums)%2==1:
+            if nums[-1]>max:
+                max=nums[-1]
+            if nums[-1]<min:
+                min=nums[-1]
+        return max,min
+X=MinMAx()
+print(X.MinMAxFinding([2]))
+                
+            

36868220e8f6864dcb1f45e12cd7755e50e7bf84
Vaibhav Dhoke <vaibhavdhoke1@gmail.com>
2021-02-03 18:40:04 -0500

    Completed sprint Array-2

--- NumberDissapeared.java
+++ NumberDissapeared.java
@@ -1,12 +1,20 @@
+// Time Complexity :O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+
 import java.util.ArrayList;
 import java.util.List;
-
 class NumberDissapeared {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         
         List<Integer> list = new ArrayList<>();
         
-        
+        //While traversing we are just making the 
         for(int i=0;i<nums.length;i++){
             int index = Math.abs(nums[i]);
             

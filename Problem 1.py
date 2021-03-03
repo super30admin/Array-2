f265ce39a244563534c28a3d58502438ddb399c3
Tirthaj Rikame <tirthaj.rikame@gmail.com>
2021-03-03 16:36:05 +0530

    Completed Numbers disappeared in array problem.

---
+++ Problem 1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        res = [] * len(nums)
+        # Edge case
+        if nums == None or len(nums) == 0:
+            return res
+
+        # Logic
+        for i in range(len(nums)):
+            idx = abs(nums[i])
+            if nums[idx - 1] > 0:
+                nums[idx - 1] = nums[idx - 1] * -1
+
+        for j in range(len(nums)):
+            if nums[j] < 0:
+                continue
+            else:
+                res.append(j + 1)
+        return res

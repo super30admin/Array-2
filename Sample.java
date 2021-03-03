c4dc011bd053cceb926bb64ae01bd35487dce06b
Mannat Dev <dev.m@husky.neu.edu>
2021-03-02 21:48:43 -0500

    Completed Problem-1

--- Sample.java
+++ Sample.java
@@ -1,7 +1,29 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity :O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
 
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        
+        if(nums == null || nums.length == 0) return result;
+        //at every index, marking nums[i]-1 as negative
+        for(int i=0; i<nums.length; i++) {
+            int index = Math.abs(nums[i])-1;
+            if(nums[index]>0){
+                nums[index] *= -1;
+            }
+        }
+        //return numbers corresponding to indices with positive values
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i]>0) {
+                result.add(i+1);
+            }
+        }
+        
+        return result;
+    }
+}

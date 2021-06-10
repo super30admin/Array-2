7e2259304d4dc45c2fbf31dc5bf3a9138638ef29
Dhiren Paryani <dhirenparyani@Dhirens-MacBook-Pro.local>
2021-06-09 17:26:12 -0700

    Completed Array-2

--- DisappearedNumbers.java
+++ DisappearedNumbers.java
@@ -1,9 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
 
 public class DisappearedNumbers {
     public static List<Integer> findDisappearedNumbers(int[] nums) {
         //change state of element corresponding to that index to -1
+        //record all elements that are positive
         List<Integer> result=new ArrayList<>();
         for(int i=0;i<nums.length;i++)
         {

ad5c6d8c7c5b402d0d786ac822ade6400b7fb938
ranepr <prasadrane@users.noreply.github.com>
2021-06-13 10:52:14 -0400

    Correcting file extension

--- Problem-2.java
+++ Problem-2.java
@@ -12,62 +12,62 @@ import java.util.List;
 // Finally we revert back to 1 and 0 removing temporary state
 class Solution {
     public void gameOfLife(int[][] board) {
-        if(board == null || board.length == 0 || board[0].length == 0) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
             return;
         }
-        
+
         int m = board.length;
         int n = board[0].length;
         int lives = 0;
-        
+
         // temporary state for new state:
         // 1 -> 0 -> 2 (becomes dead)
         // 0 -> 1 -> 3 (becomes alive)
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 lives = getSurroundLivesCount(board, i, j);
-                
-                if(board[i][j] == 1) {
-                    if(lives < 2 || lives > 3) {
+
+                if (board[i][j] == 1) {
+                    if (lives < 2 || lives > 3) {
                         // becomes dead
                         board[i][j] = 2;
                     }
                 } else {
-                    if(lives == 3) {
+                    if (lives == 3) {
                         // becomes alive
                         board[i][j] = 3;
                     }
-                }                                
+                }
             }
         }
-        
+
         // here we change the temporary state to it's actual state
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {                                
-                if(board[i][j] == 2) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 2) {
                     board[i][j] = 0;
                 } else if (board[i][j] == 3) {
                     board[i][j] = 1;
-                }                                
+                }
             }
-        }        
+        }
     }
-    
-    private int getSurroundLivesCount(int[][] board, int i, int j) {        
+
+    private int getSurroundLivesCount(int[][] board, int i, int j) {
         // we create directions array
-        int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, -1}};
-        
+        int[][] dirs = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 1 }, { 1, 1 }, { 1, -1 }, { -1, -1 } };
+
         int count = 0;
         int r = 0, c = 0;
-        for(int[] dir : dirs) {            
+        for (int[] dir : dirs) {
             r = i + dir[0];
             c = j + dir[1];
-                        
-            if(r >= 0 && r < board.length && c >= 0 && c < board[0].length && (board[r][c] == 1 || board[r][c] == 2)) {
+
+            if (r >= 0 && r < board.length && c >= 0 && c < board[0].length && (board[r][c] == 1 || board[r][c] == 2)) {
                 count++;
             }
         }
-        
+
         return count;
     }
 }

bba147f449b551b3f56ba7537fcdac87c39c50e4
SrinishRao <69674589+SrinishRao@users.noreply.github.com>
2021-06-24 12:18:54 +0530

    Completed Array2 - Please Review

---
+++ Problem1.java
@@ -0,0 +1,26 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        if(nums == null || nums.length == 0) return result;
+        for(int i=0; i<nums.length; i++){
+            int index = Math.abs(nums[i])-1;
+            if(nums[index] > 0){
+                nums[index] *= -1;
+            }
+        }
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>0){
+                result.add(i+1);
+            }else{
+                nums[i] *= -1;
+            }
+        }
+        return result;
+    }
+}
+

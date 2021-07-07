a697b0640c7e672382410e4c06c5101e952da242
Srirag <78050630+sriragcs@users.noreply.github.com>
2021-07-06 20:29:24 -0700

    Optimized the algorithm for problem1

--- problem1/DisappearedNumbers.java
+++ problem1/DisappearedNumbers.java
@@ -10,7 +10,38 @@ import java.util.List;
 import java.util.Map;
 
 public class DisappearedNumbers {
+	/* ------------------------- InPlace ------------------------- */
 	public List<Integer> findDisappearedNumbers(int[] nums) {
+		List<Integer> ans = new ArrayList<Integer>();
+		if (nums == null || nums.length == 0) {
+			return ans;
+		}
+
+		int n = nums.length;
+		for (int i = 0; i < n; i++) {
+			int idx = Math.abs(nums[i]) - 1;
+			if (nums[idx] > 0) {
+				nums[idx] *= -1;
+			}
+		}
+
+		for (int i = 1; i <= n; i++) {
+			if (nums[i - 1] > 0) {
+				ans.add(i);
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (nums[i] < 0) {
+				nums[i] *= -1;
+			}
+		}
+
+		return ans;
+	}
+
+	/* ------------------------- HashMap ------------------------- */
+	public List<Integer> findDisappearedNumbersHashMap(int[] nums) {
 		List<Integer> ans = new ArrayList<Integer>();
 		if (nums == null || nums.length == 0) {
 			return ans;

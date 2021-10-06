8766b493bbbace34d354c859bf27df7be9da111e
panth1921 <panthdev96@gmail.com>
2021-10-06 00:03:24 -0400

    array-2

---
+++ findDisappearedNumbers.swift
@@ -0,0 +1,22 @@
+// Time Complexity : 0(n)
+// Space Complexity : 0(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+    func findDisappearedNumbers(_ nums:  [Int]) -> [Int] {
+    var nums = nums
+    var output = [Int]()
+       for i in 0..<nums.count {
+           if nums[abs(nums[i])-1] > 0 {
+               nums[abs(nums[i])-1] = -nums[abs(nums[i])-1]
+           }
+       }
+       for i in 0..<nums.count {
+           if nums[i] > 0 {
+               output.append(i+1)
+           }
+       }
+        return output
+    }
---
+++ gameoflife.swift
@@ -0,0 +1,80 @@
+// Time Complexity : 0(m*n)
+// Space Complexity : 0(m*n)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+    func gameOfLife(_ board: inout [[Int]]) -> [[Int]]  {
+        var newBoard = board
+       for row in 0..<board.count {
+           for col in 0..<board[row].count {
+                print(row,col)
+               if board[row][col] == 1 {
+                   liveCellNextState(board,&newBoard,row,col)
+               } else {
+                    deadCellNextState(board,&newBoard,row,col)
+               }
+           }
+       }
+        board = newBoard
+        return newBoard
+    }
+    func liveCellNextState(_ board: [[Int]], _ newboard: inout [[Int]], _ row:Int, _ col: Int) {
+        if neighborLiveCellCount(board,row,col) < 2 {
+            newboard[row][col] = 0
+        } else if neighborLiveCellCount(board,row,col) > 3 {
+            newboard[row][col] = 0
+        }
+    }
+    func deadCellNextState(_ board: [[Int]], _ newboard: inout [[Int]], _ row:Int, _ col: Int) {
+       if neighborLiveCellCount(board,row,col) == 3 {
+            newboard[row][col] = 1
+       }
+    }
+    func neighborLiveCellCount(_ board: [[Int]], _ row:Int, _ col: Int) -> Int {
+        var liveCellCount = 0
+        if row - 1 >= 0 && col - 1 >= 0 {
+            if board[row-1][col-1] == 1 {
+                liveCellCount += 1
+            }
+        }
+         if row - 1 >= 0 {
+            if board[row-1][col] == 1 {
+                liveCellCount += 1
+            }
+        }
+         if row - 1 >= 0 && col + 1 < board[row].count {
+           if board[row-1][col+1] == 1 {
+                liveCellCount += 1
+            }
+        }
+         if col - 1 >= 0 {
+           if board[row][col-1] == 1 {
+                liveCellCount += 1
+            }
+        }
+         
+         if row + 1 < board.count && col - 1 >= 0 {
+            if board[row+1][col-1] == 1 {
+                liveCellCount += 1
+            }
+        }
+        if row + 1 < board.count {
+           if board[row+1][col] == 1 {
+                liveCellCount += 1
+            }
+        }
+        if row + 1 < board.count && col + 1 < board[row].count {
+            if board[row+1][col+1] == 1 {
+                liveCellCount += 1
+            }
+        }
+        if col + 1 < board[row].count {
+           if board[row][col+1] == 1 {
+                liveCellCount += 1
+            }
+        }
+        return liveCellCount
+        
+    }

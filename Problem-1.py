8a85a79ae98bd4e7acf43cf421d5df7cb47810cf
Adhish Shrivastava <adhish70@gmail.com>
2021-10-20 15:16:46 -0400

    Initial commit

---
+++ Problem-1.py
@@ -0,0 +1,9 @@
+# 448. Find All Numbers Disappeared in an Array
+# https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+# Time Complexiety: 
+# Space Complexiety: 
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        pass
---
+++ Problem-2.py
@@ -0,0 +1,12 @@
+# 289. Game of Life
+# https://leetcode.com/problems/game-of-life/
+
+# Time Complexiety: 
+# Space Complexiety: 
+
+class Solution:
+    def gameOfLife(self, board) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        pass

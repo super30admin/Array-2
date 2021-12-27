adc17e7d491f4f9088d7c351e6c743bff7569c8c
Gauri <sarpotdar.g@husky.neu.edu>
2021-12-27 13:44:19 -0500

    All done

---
+++ DisappearedNumbers.java
@@ -0,0 +1,29 @@
+// Time Complexity :O(N)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this : No
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length ; i++){
+            int index = Math.abs(nums[i])-1;
+            if(nums[index] > 0){
+                nums[index] = -1 * nums[index];
+            }
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] < 0){
+                nums[i] = -1 * nums[i];
+            }
+            else{
+                result. add(i+1);
+            }
+        }
+        return result;
+    }
+}
---
+++ gameoflife.java
@@ -0,0 +1,61 @@
+// Time Complexity :O(N*M)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this : No
+
+class Solution {
+    int m,n;
+    public void gameOfLife(int[][] board) {
+        if(board == null || board.length == 0) return;
+        m = board.length;
+        n = board[0].length;
+
+        //1 -->0 ==> 2
+        //0 -->1 ==> 3
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                int lives = countlives(board ,i , j);
+                if(board[i][j] == 1){
+                    if(lives < 2 || lives > 3){
+                        board[i][j] = 2;
+                    }
+                }
+                else{
+                    if(lives == 3){
+                        board[i][j] = 3;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0;i < m ; i++){
+            for(int j = 0; j < n;j++){
+                if(board[i][j] == 2){
+                    board[i][j] = 0;
+                }
+                else{
+                    if(board[i][j] == 3){
+                        board[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    private int countlives(int[][] board, int r, int c){
+        int[][]dirs = {{0,1},{0,-1},{1,0},{-1,0},{1,1},{-1,-1},{-1,1},{1,-1}};
+        int count = 0;
+        for(int[]dir : dirs){
+            int nr = r + dir[0];
+            int nc = c + dir[1];
+            if(nr >= 0 && nr < m && nc >= 0 && nc < n && (board[nr][nc] == 1 || board[nr][nc] == 2))             {
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+}

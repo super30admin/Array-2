7dcb28f8cbcb01c2ee50a6f7c11fa32be1dbc213
vinayak sonandkar <sonandkarvinayak@gmail.com>
2022-02-19 16:42:53 -0800

    Array-2: Completed

--- Find All Numbers Disappeared in an Array.py
+++ Find All Numbers Disappeared in an Array.py
@@ -1,41 +1,13 @@
-#TC:O(M*N), where M,N are row and column of matrix
-#SC:O(1)
+#TC:O(n)
+#SC:O(1)   
 class Solution:
-    def valid(self,board, x, y):
-        if (x>=0 and x<len(board)) and (y>=0 and y<len(board[0])):
-            return True
-        else:
-            return False
-               
-    def noOfAliveCell(self, board,r,c):
-        mapping = [[1,0],[-1,0],[0,1], [0,-1],[1,1],[-1,-1],[1,-1],[-1,1]]
-        count = 0
-        for i in range(len(mapping)):
-            x = r + mapping[i][0]
-            y = c + mapping[i][1]
-            if self.valid(board,x,y):
-                if board[x][y] == -2 or board[x][y] == 1:
-                    count += 1
-        return count
-            
-    def gameOfLife(self, board: List[List[int]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                live = self.noOfAliveCell(board,i,j)
-                if board[i][j] == 1:
-                    if live < 2 or live > 3:
-                        board[i][j] = -2
-                if board[i][j] == 0:
-                    if live == 3:
-                        board[i][j] = -1
-                        
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == -2:
-                    board[i][j] = 0
-                if board[i][j] == -1:
-                    board[i][j] = 1
-        
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        ans = [ ]
+        for i in range(len(nums)):
+            temp = abs(nums[i]) - 1
+            if nums[temp] > 0:
+                nums[temp] *= -1
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                ans.append(i+1)
+        return ans

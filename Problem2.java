f6a5b1a32d9f486ef958d26c60efad0efc7d75a6
Aryyama Kumar Jana <aryyamakumarjana@MacBook-Pro.local>
2022-02-22 01:22:25 -0700

    Completed Array-2

--- Problem2.java
+++ Problem2.java
@@ -1,4 +1,4 @@
-// Time Complexity : O(m*n) where m = number of rows in board, n = number of columns in board
+// Time Complexity : O(n) where n = length of array
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
@@ -9,76 +9,41 @@
 import java.util.*;
 public class Problem2 {
 
-    private static int[][] directions = new int[][]{
-        {-1, -1}, // left up
-        {-1, 0}, // up
-        {-1, 1}, // right up
-        {0, 1}, // right
-        {1, 1}, // right down
-        {1, 0}, // down
-        {1, -1}, // left down
-        {0, -1} // left
-        
-    };
-    
-    private static int findliveneighbors(int[][] board, int i, int j) {
-        int rows = board.length;
-        int cols = board[0].length;
-        int count = 0; 
-        for (int[] direction: directions) {
-            int row = i + direction[0];
-            int col = j + direction[1];
-            if (row >= 0 && row < rows && col >= 0 && col < cols) {
-                if (board[row][col] == 1 || board[row][col] == -2) {
-                    count++;
-                }
-            }
+    public static int[] getMinMax(int[] arr) {
+        int n = arr.length;
+        // ans[0] = min, ans[1] = max
+        int[] ans = new int[2];
+        int i;
+        // If size of array is odd, set initial min and max as first element in the array
+        if (n%2 == 1) {
+            ans[0] = arr[0];
+            ans[1] = arr[0];
+            // Set pointer to 2nd index (i = 1)
+            i = 1;
+        } else { // If size of array is even, set initial min and max as min(first element, second element) and max(first element, second element) respectively
+            ans[0] = Math.min(arr[0], arr[1]);
+            ans[1] = Math.max(arr[0], arr[1]);
+            // Set pointer to 3rd index (i = 2)
+            i = 2;
         }
-        return count;
-        
-        
-    }
-    
-    public static void gameOfLife(int[][] board) {
-        // 0 to 1 => -1
-        // 1 to 0 => -2
-        // 0 to 0 => 0
-        // 1 to 1 => 1
-        int rows = board.length;
-        int cols = board[0].length;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                int liveneighbors = findliveneighbors(board, i, j);
-                if (board[i][j] == 1) { // live cell
-                    if (liveneighbors < 2 || liveneighbors > 3) {
-                        board[i][j] = -2;
-                    }
-                } else {
-                    if (liveneighbors == 3) {
-                        board[i][j] = -1;
-                    }
-                }
+        // Make pairs and compare among each other and then with min and max
+        while(i < n-1) {
+            if (arr[i] < arr[i+1]) {
+                ans[0] = Math.min(ans[0], arr[i]);
+                ans[1] = Math.max(ans[1], arr[i+1]);
+            } else {
+                ans[0] = Math.min(ans[0], arr[i+1]);
+                ans[1] = Math.max(ans[1], arr[i]);
             }
+            i += 2;
         }
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (board[i][j] == -1) {
-                    board[i][j] = 1;
-                }
-                if (board[i][j] == -2) {
-                    board[i][j] = 0;
-                }
-            }
-        }
-            
+        return ans;
     }
 
     public static void main(String[] args) {
-        int[][] board = {{0,1,0},{0,0,1},{1,1,1},{0,0,0}};
-        gameOfLife(board);
-        for (int i = 0; i < board.length; i++) {
-            System.out.println(Arrays.toString(board[i]));
-        }
+        int[] arr = {1000, 11, 445, 1, 330, 3000};
+        int[] ans = getMinMax(arr);
+        System.out.println(Arrays.toString(ans));
     }
 
 

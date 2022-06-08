ce2f87a06baf71408127a73b55039ecab7243248
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-08 16:52:55 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -36,4 +36,77 @@ class Solution {
     }
     
 }
-// Your code here along with comments explaining your approach
+
+//****GAME OF LIFE****
+// Time Complexity :O(m*n)*8 i.e: o(m*n)
+// Space Complexity :o(1)
+// Did this code successfully run on Leetcode :Y
+// Any problem you faced while coding this :N
+
+
+class Solution {
+    int m;
+    int n;
+    int[][] dir;
+    
+    public void gameOfLife(int[][] board) {
+        m=board.length;
+        n=board[0].length;
+        //              {right} {left} {down} {up}   {upleft}{upright}{downleft}{downright}//
+        dir= new int[][]{{0,1},{0,-1},{1,0}, {-1,0}, {-1,-1}, {-1,1},  {1,-1}, {1,1}};
+        
+        
+        //1..0  > 3
+        //0..1  > 2
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int countalive=countalive(board, i, j);
+                if(board[i][j]==1 &&(countalive<2 || countalive>3) )
+                {
+                    board[i][j]=3;
+                }
+                if(board[i][j]==0 && countalive==3)
+                {
+                    board[i][j]=2;
+                }
+            }
+        }
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(board[i][j]==3)
+                {
+                    board[i][j]=0;
+                    
+                }
+                if(board[i][j]==2)
+                {
+                    board[i][j]=1;
+                    
+                }
+                
+            }
+        }
+        
+    }
+    
+    private int countalive(int[][] board, int i, int j)
+    {
+        int res=0;
+        
+        for(int[] d: dir)
+        {
+            int nr=i+ d[0];
+            int nc=j+ d[1];
+            if(nr>=0 && nc>=0 && nr<m && nc<n && (board[nr][nc]==1 || board[nr][nc]==3))
+            {
+                res++;
+            }
+        }
+    return res;
+    }
+}

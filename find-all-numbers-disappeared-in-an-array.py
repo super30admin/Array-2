a4d254aabf579dc233ffc4bb44260fd9f956f2dc
Sandeep Kumar Reddy Onteddy (Student) <sonteddy@sundevils.asu.edu>
2022-06-08 10:29:05 -0700

    Completed Array-2 Please Review

---
+++ find-all-numbers-disappeared-in-an-array.py
@@ -0,0 +1,13 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        res=[]
+        for i in range(len(nums)):
+            idx=abs(nums[i])-1
+            if nums[idx]>0:
+                nums[idx]*=-1
+        for i in range(len(nums)):
+            if nums[i]>0:
+                res.append(i+1)
+        return res

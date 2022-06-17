994ac556c96df986f8178aaa2cad528f8a1a3645
Dixika Grewal <59345244+dixg@users.noreply.github.com>
2022-06-17 00:24:56 -0700

    done

--- numDisapppeared.py
+++ numDisapppeared.py
@@ -1,3 +1,8 @@
+# Time Complexity : log(n)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : yes
+# Any problem you faced while coding this : No
+
 class Solution(object):
     def findDisappearedNumbers(self, nums):
         for i in range(len(nums)):

700929cafff9205f40d8a33455a3e2bf6368dae6
Jayavani Akkiraju <jayavaniakkiraju@Jayavanis-MacBook-Pro.local>
2022-06-24 07:53:34 +0530

    Array-2 completed

--- gameOfLife.py
+++ gameOfLife.py
@@ -1,1 +1,49 @@
+'''
+- Passed testcases on leetcode
+'''
+class Solution:
 
+    def gameOfLife(self, board):
+        """
+        :type board: List[List[int]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+
+        # O(m*n) Time Complexity ; O(1) Space Complexity
+        m = len(board)
+        if (board == None or m == 0):
+            return
+        n = len(board[0])
+
+        for r in range(m):
+            for c in range(n):
+                Ones = self.Ones(board, m, n, r, c)
+                if (board[r][c] == 1 and (Ones < 2 or Ones > 3)):
+                    board[r][c] = 2
+                if (board[r][c] == 0 and Ones == 3):
+                    board[r][c] = 3
+
+        for r in range(m):
+            for c in range(n):
+                if (board[r][c] == 2):
+                    board[r][c] = 0
+                if (board[r][c] == 3):
+                    board[r][c] = 1
+
+        return
+
+    def Ones(self, board, m, n, r, c):
+
+        # O(1) Time Complexity | O(1) Space Complexity
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1],
+                [-1, -1], [-1, 1], [1, -1], [1, 1]]
+        cnt = 0
+
+        for i in directions:
+            row = r + i[0]
+            col = c + i[1]
+            if (row >= 0 and row < m and col >= 0 and col < n):
+                if (board[row][col] == 1 or board[row][col] == 2):
+                    cnt += 1
+
+        return cnt
--- min_max.py
+++ min_max.py
@@ -1,1 +1,38 @@
+class pair:
+    #O(n) - Time complexity where n is the size of the array; O(1) - Space complexity
+    def __init__(self):
+        self.min = 0
+        self.max = 0
+ 
+def getMinMax(arr: list, n: int) -> pair:
+    min_max = pair()
+ 
 
+    if n == 1:
+        min_max.max = arr[0]
+        min_max.min = arr[0]
+        return min_max
+ 
+  
+    if arr[0] > arr[1]:
+        min_max.max = arr[0]
+        min_max.min = arr[1]
+    else:
+        min_max.max = arr[1]
+        min_max.min = arr[0]
+ 
+    for i in range(2, n):
+        if arr[i] > min_max.max:
+            min_max.max = arr[i]
+        elif arr[i] < min_max.min:
+            min_max.min = arr[i]
+ 
+    return min_max
+ 
+# Driver Code
+if __name__ == "__main__":
+    arr = [1000, 11, 445, 1, 330, 3000]
+    arr_size = 6
+    min_max = getMinMax(arr, arr_size)
+    print("Minimum:", min_max.min)
+    print("Maximum:", min_max.max)

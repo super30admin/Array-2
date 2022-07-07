b0a46b1ed6bf7d1f924b1fbbbd621d4e0f6749c3
Trupti Joshi Satish <trupity@gmail.com>
2022-07-07 14:52:28 +0530

    Completed Sprint Array-2

---
+++ GameOfLife.java
@@ -0,0 +1,66 @@
+// Time Complexity : O(mn)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    int m, n;
+    
+    public void gameOfLife(int[][] board) {
+        if(board == null){
+            return;
+        }
+        
+        m = board.length;
+        n = board[0].length;
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                // Count the live neighbors for each element
+                int countLives = countLiveNeighbors(board, i, j);
+                
+                if(board[i][j] == 0){
+                    // Marking it with a different number so that it doesn't interfere with the further computation
+                    if(countLives == 3){
+                        board[i][j] = 3;
+                    }
+                }
+                else{
+                    if(countLives < 2 || countLives > 3){
+                        board[i][j] = 2;
+                    }
+                }
+            }
+        }
+        
+        // Reverting the markings
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == 3){
+                    board[i][j] = 1;
+                }
+                else if(board[i][j] == 2){
+                    board[i][j] = 0;
+                }
+            }
+        }
+    }
+    
+    private int countLiveNeighbors(int[][] board, int row, int column){
+        int[][] dirs = {{-1,-1}, {-1,0}, {-1,1}, {0,-1}, {0,1}, {1,-1}, {1,0}, {1,1}};
+        int count = 0;
+        
+        for(int[] dir : dirs){
+            int newRow = row + dir[0];
+            int newCol = column + dir[1];
+            
+            if(newRow >= 0  && newRow < m && newCol >= 0 && newCol < n && (board[newRow][newCol] == 1 || board[newRow][newCol] == 2)){
+                count++;
+            }
+        }
+        return count;
+    }
+}

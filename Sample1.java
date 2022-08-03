5b03dee6e68791adbee7f952e55458bb200fcb30
RAHUL UDHAYAKUMAR <65881610+RahulDeMaverick@users.noreply.github.com>
2022-08-02 22:40:12 -0400

    problem2

--- Sample1.java
+++ Sample1.java
@@ -1,7 +1,30 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : o(n)
+// Space Complexity : o(1)
+// Did this code successfully run on Leetcode : yes 
+// Any problem you faced while coding this : no
 
 
 // Your code here along with comments explaining your approach
+
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        
+        List<Integer> res = new ArrayList<>();
+        
+        for(int i=0;i<nums.length;i++){ 
+            int index = Math.abs(nums[i])-1;
+            if(nums[index] >0)
+                nums[index] *= -1;
+        }
+        
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] >0)
+                res.add(i+1);
+            else
+                nums[i] *= -1;
+            }
+        return res;
+    }
+    
+}

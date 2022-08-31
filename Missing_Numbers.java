bf29738e561695c006a8e4601ce9dfd0c5e567ba
ShreshtaBalmuri <78928725+ShreshtaBalmuri@users.noreply.github.com>
2022-08-30 18:25:27 -0700

    Create Missing_Numbers.java

---
+++ Missing_Numbers.java
@@ -0,0 +1,22 @@
+//TimeComplexity O(n)
+//SpaceComplexity O(1)
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0 ; i < nums.length ; i++){
+            int index = Math.abs(nums[i])-1;
+            if(nums[index]>0){
+                 nums[index] = -1*nums[index];
+            }
+
+        }
+
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] > 0){
+                result.add(i+1);
+            }
+        }
+        return result;
+    }
+}

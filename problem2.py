dcb9c24434d30f3232b5fe465ea300f53e324c90
Yashnaredi <yashnaredi26@gmail.com>
2022-09-27 14:02:31 -0400

    completed array-2

---
+++ problem2.py
@@ -0,0 +1,36 @@
+#MinMax
+#SC:O(1)
+#TC:O(N)
+def getMinMax(arr):
+	
+	n = len(arr)
+	
+	if(n % 2 == 0):
+		mx = max(arr[0], arr[1])
+		mn = min(arr[0], arr[1])
+		
+	
+		i = 2
+		
+	
+	else:
+		mx = mn = arr[0]
+		
+	
+		i = 1
+		
+
+	while(i < n - 1):
+		if arr[i] < arr[i + 1]:
+			mx = max(mx, arr[i + 1])
+			mn = min(mn, arr[i])
+		else:
+			mx = max(mx, arr[i])
+			mn = min(mn, arr[i + 1])
+			
+		
+		i += 2
+	
+	return (mx, mn)
+	
+

b3ff7d75c3c365bf3c40cc02af927386b136acee
Sumanth Shankar <sumanth@rezy360.com>
2022-10-01 14:51:18 -0700

    Solution for disappeared numbers

--- DisappearedNumbersSolution.java
+++ DisappearedNumbersSolution.java
@@ -1,6 +1,32 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
-public class DisappearedNumbersSolution {}
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisappearedNumbersSolution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            int num = Math.abs(nums[i]);
+            num--;
+            if(nums[num] > 0) {
+                nums[num] = nums[num] * -1;
+            }
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+        
+        return result;
+    }
+}

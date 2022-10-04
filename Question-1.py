5b8389517d5961283bfa6aba81e1756527ea2e2b
Jas Bhavesh Zaveri <Zaveri.Jas@cognizancetech.com>
2022-10-04 18:02:05 -0500

    Completed Sprint Array-2

---
+++ Question-1.py
@@ -0,0 +1,11 @@
+#Time Complexity : O(n)
+#Space Complexity : O(1)
+#Did this code successfully run on Leetcode : Yes
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        output = [i+1 for i in range(len(nums))]
+        for num in nums:
+            output[num-1]=0
+        output = [i for i in output if i != 0]
+        return output

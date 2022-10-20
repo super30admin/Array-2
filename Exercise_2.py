21d62220bdbb9e6edaf889bd809a161682da2472
Tanaya-Mahakalkar <tanaya.mahakalkar95@gmail.com>
2022-10-20 00:27:14 -0500

    Completed - Array 2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,12 @@
+'''
+Time Complexity : O(n)
+Space Complexity : O(1)
+Did this code successfully run on Leetcode : Yes
+Any problem you faced while coding this : No
+'''
+
+
+
 class Solution:
  
     def gameOfLife(self, board: List[List[int]]) -> None:

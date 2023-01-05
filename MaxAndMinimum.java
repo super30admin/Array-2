1254009ac96994ecb955aa1211db0bdf440654e9
Shridhar Sattur <ssattur@ssattur-mn1.linkedin.biz>
2023-01-04 17:18:54 -0800

    Completed - Array-2.

---
+++ MaxAndMinimum.java
@@ -0,0 +1,13 @@
+public class MaxAndMinimum {
+  class Pair {
+    int min;
+    int max;
+  }
+
+
+
+   Pair getMinMax(int arr[], int n) {
+    Pair minmax = new  Pair();
+    return minmax;
+  }
+}

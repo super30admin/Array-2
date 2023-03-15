a6d263dcf944e5717f26ae223aa4c0079d1ad218
akhilgour <akhilgour209@gmail.com>
2023-03-14 17:12:44 -0700

    Update DisappearedNumbers.java

--- DisappearedNumbers.java
+++ DisappearedNumbers.java
@@ -1,3 +1,7 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+
 class DisappearedNumbers {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         ArrayList<Integer> list = new ArrayList<>();
@@ -8,6 +12,31 @@ class DisappearedNumbers {
         for(int i = 1; i <= nums.length; i++){
             if(!hs.contains(i)){
                 list.add(i);
+            }
+        }
+        return list;
+    }
+}
+
+
+
+
+
+//Second solution using O(n) time but O(1) space
+class DisappearedNumbers {
+public List<Integer> findDisappearedNumbers(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(nums[Math.abs(nums[i])-1]>0)
+            nums[Math.abs(nums[i])-1] = -1 * nums[Math.abs(nums[i])-1];
+        }
+        for(int i = 0; i <= n-1; i++){
+            if(nums[i]<0){
+                nums[i] = Math.abs(nums[i]);
+            }
+            else{
+                list.add(i+1);
             }
         }
         return list;

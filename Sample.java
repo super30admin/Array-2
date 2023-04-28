010e1a998a9ca7c3c5591c9a91187439c2902760
Pranay Joshi <pranay@Pranays-MBP.attlocal.net>
2023-04-28 05:32:22 -0500

    Added Solution for Game_of_live question - Please Review

--- Sample.java
+++ Sample.java
@@ -64,3 +64,60 @@ class Solution {
         nums[j] = temp;
     }
 }
+
+## Problem3 (https://leetcode.com/problems/game-of-life/)
+
+// Time Complexity - 0(m * n) because the method iterates through all the cells in the board twice, 
+//                   once for updating the state of each cell and once for converting the temporary values back to the original values.
+// Space Complexity - 0(1) because the method only uses a constant amount of extra space to store the count_live.
+
+class Solution {
+    int m, n;
+    public void gameOfLife(int[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+        m = board.length;
+        n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int count_live = countlive(board, i, j);
+                if (board[i][j] == 1) {                           // if its a live cell
+                    if (count_live < 2 || count_live > 3) {
+                        board[i][j] = 2;                          // live becaome dead change it to 2
+                    }
+                }                 
+                else if (board[i][j] == 0) {                      // if its a dead cell
+                    if (count_live == 3) {
+                        board[i][j] = 3;
+                    }            
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 2) {
+                    board[i][j] = 0;
+                }
+                else if (board[i][j] == 3) {
+                    board[i][j] = 1;
+                }
+            }
+        }
+    }
+    private int countlive (int board[][], int row, int col) {
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+        int count = 0;
+        
+        for (int i = 0; i < 8; i++) {
+            int nr = row + dirs[i][0];
+            int nc = col + dirs[i][1];
+            if (nr >= 0 && nr < m && nc >= 0 && nc < n && (board[nr][nc] == 1 || board[nr][nc] == 2)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}

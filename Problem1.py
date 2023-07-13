ac7a27091f74ae06362b3047ab2bf347423afb16
NisargaVenkatesh <venkatesh.ni@northeastern.edu>
2023-07-12 21:10:55 -0400

    Exercise for review

--- Problem1.py
+++ Problem1.py
@@ -18,3 +18,13 @@ s = Solution()
 input_nums = [4, 3, 2, 7, 8, 2, 3, 1]
 output_nums = s.findDisappearedNumbers(input_nums)
 print(output_nums)
+
+solution = Solution()
+board = [
+    [0, 1, 0],
+    [0, 0, 1],
+    [1, 1, 1],
+    [0, 0, 0]
+]
+solution.gameOfLife(board)
+print(board)

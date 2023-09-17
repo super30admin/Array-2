663392e38e1001e61a8868229ea9a38dac8cbc8e
Pratiksha Kapse <pratiksha@Pratikshas-MacBook-Pro.local>
2023-09-17 16:21:49 -0700

    adds game of life and missing numbers

--- problem1.cpp
+++ problem1.cpp
@@ -1,22 +1,40 @@
+//Time complexity : O(n)
+//Space complexity: O(1)
+
+//Aproach: store values from 1 to n in a set
+// then traverse over array and if you find a number in set
+//simply erase it in set and then at the end of the iteration 
+//whaterver is remained will be stored in array of missing numebers
+
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        
-        
-        vector<int> res;
-        unordered_map<int,bool> map;
-        
+     /*   unordered_map<int,int> counter; //extra space
+        vector<int> res; 
+        for(auto n: nums)
+        {
+            counter[n]++;
+        }
+
         for(int i=0;i<nums.size();i++)
         {
-         map[nums[i]]=true;   
-        }
-        for(int i=1;i<=nums.size();i++)
-        {
-            if(!map.count(i))
+            if(!counter.count(i+1))
             {
-                res.push_back(i);
+                res.push_back(i+1);
             }
+        }*/
+         vector<int> res; 
+        unordered_set<int> set
+        for(int i=0;i<nums.size();i++){
+            set.insert(i+1);
         }
+        for(auto n:nums)
+        {
+            if(set.count(n))
+                set.erase(n);
+        }
+        for(auto s:set)
+            res.push_back(s);
         return res;
     }
 };
--- problem2.cpp
+++ problem2.cpp
@@ -1,61 +1,72 @@
+//Time complexity: O(m*n)
+//Space complexity: O(1)
+//Approach: 
+//1. Go over each cell of the matrix 
+//2. have the count of live cell around it ready
+//  a. get the current cell coords
+//  b. iterate over dirs and create new coords
+//  c. check if these new coords are valid
+//  d. increment the count if valid and return in the end
+//3. then check who am i, am I 1 or0?
+//  a. if I am 1 and if live cells around me are less than 2 and more than 3 ->I die
+//  b. if I am 0 and live cells around me is exactly 3 I live 
+//4. re-ierate over this baord again to update 2 and 3 to 0 and 1 respectively
+//5. Done.
 class Solution {
 public:
+vector<vector<int>> dirs{{0,-1},{-1,0},{0,1},{1,0},{-1,-1},{1,1},{-1,1},{1,-1}};
+
     void gameOfLife(vector<vector<int>>& board) {
-        
         int m=board.size();
         int n=board[0].size();
-        
-        for(int r=0;r<m;r++)
+//idea: 
+//1->0 =>2
+//0->1 =>3
+
+        for(int i=0;i<m;i++)
         {
-            for(int c=0;c<n;c++)
+            for(int j=0;j<n;j++)
             {
-                int count=countLives(board,r,c,m,n);
-                if(board[r][c]==1 and (count<2 or count>3))
+                int countOfLiveCells=countFunc(board, i, j);
+                if(board[i][j]==1)
                 {
-                    board[r][c]=3;
+                    if(countOfLiveCells<2 || countOfLiveCells>3)
+                        board[i][j]=2;
                 }
-                if(board[r][c]==0 and count==3)
+                else if(board[i][j]==0)
                 {
-                    board[r][c]=2;
+                    if(countOfLiveCells==3)
+                        board[i][j]=3;
                 }
             }
         }
-        
-        for(int r=0;r<m;r++)
+
+        for(int i=0;i<m;i++)
         {
-            for(int c=0;c<n;c++)
+            for(int j=0;j<n;j++)
             {
-                if(board[r][c]==3)
-                {
-                    board[r][c]=0;
-                }
-                if(board[r][c]==2)
-                {
-                    board[r][c]=1;
-                }
+                if(board[i][j]==2)
+                    board[i][j]=0;
+                else if(board[i][j]==3)
+                    board[i][j]=1;
             }
         }
-        
-        
     }
-    
-    int countLives(vector<vector<int>> board, int i, int j, int m, int n)
+    int countFunc(vector<vector<int>>& board, int r, int c)
     {
         int count=0;
-        vector<vector<int>> dirs={{0,1},{0,-1},{1,0},{-1,0},{1,-1},{-1,1},{-1,-1},{1,1}};
-        for(int k=0; k<dirs.size(); k++)
+        for(auto dir:dirs)
         {
-             int r= i+ dirs[k][0];
-                   int c= j+ dirs[k][1];
-            if(r<m and c<n and r>=0 and c>=0 and (board[r][c]==1 || board[r][c]==3))
-               {
-                   count++;
-                  
-               }
+            int newR=r+dir[0];
+            int newC=c+dir[1];
+            if(valid(board, newR,newC))
+                count++;
         }
         return count;
     }
+    bool valid(vector<vector<int>>& board, int r, int c)
+    {
+        return r>=0 and r<board.size() and c>=0 and c<board[0].size() and (board[r][c]==1 || board[r][c]==2);
+    }
 };
 
-//Time complexity - O(mxn) ignoring constant iteration over direction matrix
-//Space is indeed O(1)
